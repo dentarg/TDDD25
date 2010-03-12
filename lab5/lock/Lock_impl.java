1c648eb86b167c95fabcf070ed939b67a6e9d724
Patrik Ragnarsson <patrik@starkast.net>
2010-03-12 01:32:45 +0100

    Tabs to spaces

--- lab5/lock/Lock_impl.java
+++ lab5/lock/Lock_impl.java
@@ -18,18 +18,18 @@ import java.lang.Thread;
 
 public class Lock_impl extends LockPOA
 {
-	// Declaration of the array that stores the remote references
-	Lock[] objArray;
+   // Declaration of the array that stores the remote references
+   Lock[] objArray;
 
-	//
-	// TODO:
-	// Declare necessary variables
-	private short  my_id;
-	private int    state;
-	private int    number_of_clients;
-	private int[]  token;
-	private int[]  request;
-	public static final int NO_TOKEN       = 0;
+   //
+   // TODO:
+   // Declare necessary variables
+   private short  my_id;
+   private int    state;
+   private int    number_of_clients;
+   private int[]  token;
+   private int[]  request;
+   public static final int NO_TOKEN       = 0;
    public static final int TOKEN_PRESENT  = 1;
    public static final int TOKEN_HELD     = 2;
    public static final boolean DEBUG      = false;
@@ -43,7 +43,7 @@ public class Lock_impl extends LockPOA
 // ----------------------------------------------------------------------
     public Lock_impl(org.omg.PortableServer.POA poa)
     {
-	poa_ = poa;
+   poa_ = poa;
     }
 
 
@@ -53,7 +53,7 @@ public class Lock_impl extends LockPOA
     public org.omg.PortableServer.POA
     _default_POA()
     {
-	return poa_;
+   return poa_;
     }
 
 
@@ -62,37 +62,37 @@ public class Lock_impl extends LockPOA
 // ----------------------------------------------------------------------
 
 /*
-	This method is used to request the token from a remote object.
+   This method is used to request the token from a remote object.
 */
-	public void request_token(short id, int logical_time) {
-		System.out.print("Token was requested by client ");
-		System.out.println(id+" at remote logical time "+logical_time);
+   public void request_token(short id, int logical_time) {
+      System.out.print("Token was requested by client ");
+      System.out.println(id+" at remote logical time "+logical_time);
 
-		// TODO: Implement code for handling token requests from remote objects
+      // TODO: Implement code for handling token requests from remote objects
       request[id] = Math.max(request[id], logical_time);
       if(state == TOKEN_PRESENT) {
          release_token();
       }
-	}
+   }
 
 /*
-	This method is called by a remote object when it wants to hand the token to us.
+   This method is called by a remote object when it wants to hand the token to us.
 */
-	public void acquire_token(int[] token) {
-		System.out.println("Token acquired!");
+   public void acquire_token(int[] token) {
+      System.out.println("Token acquired!");
 
-		// TODO: Implement code for acquiring the token from a remote object
-		this.token = token;
+      // TODO: Implement code for acquiring the token from a remote object
+      this.token = token;
       state = TOKEN_HELD;
-	}
+   }
 
 /*
-	This function is called from any remote object that wants us to print
-	some text on the screen (for instance to check if we are alive).
+   This function is called from any remote object that wants us to print
+   some text on the screen (for instance to check if we are alive).
 */
-	public void print_message(short id) {
-		System.out.println("Received a message from client "+id);	
-	}
+   public void print_message(short id) {
+      System.out.println("Received a message from client "+id);   
+   }
 
 // ----------------------------------------------------------------------
 // Local methods that can be called only from the program that created
@@ -100,47 +100,47 @@ public class Lock_impl extends LockPOA
 // ----------------------------------------------------------------------
 
 /*
-	This method must be called when all clients are started. It finds the references
-	of remote objects and puts them into an array. It also initializes all variables
-	necesary for the distributed mutual exclusion algorithm to work.
+   This method must be called when all clients are started. It finds the references
+   of remote objects and puts them into an array. It also initializes all variables
+   necesary for the distributed mutual exclusion algorithm to work.
 */
-	public void initialize(int my_id, int number_of_clients, boolean has_token, String username) throws org.omg.CORBA.UserException {
+   public void initialize(int my_id, int number_of_clients, boolean has_token, String username) throws org.omg.CORBA.UserException {
 
-		System.out.println("Initializing "+number_of_clients+" clients...");
+      System.out.println("Initializing "+number_of_clients+" clients...");
 
-		// Initialize object array
-		objArray=new Lock[number_of_clients];
+      // Initialize object array
+      objArray=new Lock[number_of_clients];
 
-		// Get the NamingContext
-		org.omg.CORBA.Object rootObj = _orb().resolve_initial_references("NameService");
-		org.omg.CosNaming.NamingContext root = org.omg.CosNaming.NamingContextHelper.narrow(rootObj);
-		
-		for (int i=0; i<number_of_clients; i++) {
-			// Only search for remote objects
-			if (i!=my_id) {
-				// Use the CORBA name service to find the object
-				System.out.println("Searching for client: "+username+i);
-				NameComponent[] nc = new NameComponent[1];
-				nc[0] = new NameComponent("Lock",username+i);
-				org.omg.CORBA.Object obj = ((NamingContext)root).resolve(nc);
+      // Get the NamingContext
+      org.omg.CORBA.Object rootObj = _orb().resolve_initial_references("NameService");
+      org.omg.CosNaming.NamingContext root = org.omg.CosNaming.NamingContextHelper.narrow(rootObj);
+      
+      for (int i=0; i<number_of_clients; i++) {
+         // Only search for remote objects
+         if (i!=my_id) {
+            // Use the CORBA name service to find the object
+            System.out.println("Searching for client: "+username+i);
+            NameComponent[] nc = new NameComponent[1];
+            nc[0] = new NameComponent("Lock",username+i);
+            org.omg.CORBA.Object obj = ((NamingContext)root).resolve(nc);
 
-				// Store it into an array containing references to all
-				// remote objects
-				objArray[i] = LockHelper.narrow(obj);
+            // Store it into an array containing references to all
+            // remote objects
+            objArray[i] = LockHelper.narrow(obj);
 
-				// Function method() of the remote client i can now be accessed by:
-				// objArray[i].method();
-				objArray[i].print_message((short)my_id);
-			}
-		}
-		
-		// The reference corresponding to this, local, object should be null
-		// since we are not going to access our own member functions using CORBA
-		objArray[my_id]=null;		
-		System.out.println("All remote objects are located.");
-		
-		//
-		// TODO: Initialization of variables, such as the token.
+            // Function method() of the remote client i can now be accessed by:
+            // objArray[i].method();
+            objArray[i].print_message((short)my_id);
+         }
+      }
+      
+      // The reference corresponding to this, local, object should be null
+      // since we are not going to access our own member functions using CORBA
+      objArray[my_id]=null;      
+      System.out.println("All remote objects are located.");
+      
+      //
+      // TODO: Initialization of variables, such as the token.
       this.my_id = (short) my_id;
       this.number_of_clients = number_of_clients;
       token = new int[number_of_clients];
@@ -158,67 +158,67 @@ public class Lock_impl extends LockPOA
          state = NO_TOKEN;
       }
 
-	}
+   }
 
 
 /*
-	This method is called from the local client whenever it wants to acquire
-	the lock.
+   This method is called from the local client whenever it wants to acquire
+   the lock.
 */    
     public void acquire() {
-		System.out.println("Acquiring lock...");
+      System.out.println("Acquiring lock...");
 
-		//
-		// TODO:
-		// Implement code for acquiring the lock. This means requesting the token
-		// from all other clients, if we do not already have it.
-		if(state == NO_TOKEN) {
-		   for(int i = 0; i < number_of_clients; i++) {
-		      if(i != my_id) { // do not request from myself
-		         objArray[i].request_token(my_id, token[my_id] + 1);
-	         }
-		   }
-		   // wait until we receive token
-		   while(true) {
-		      if(state == TOKEN_HELD) {
-		         break;
-		      }
-		      else {
-		         try {
-		            Thread.sleep(500);
-		         }
-		         catch(InterruptedException e) {
-		            System.err.println("sleep error");
-		         }
-		      }
-		   }
-	   }
-	   state = TOKEN_HELD;
-	   // enter critical section
+      //
+      // TODO:
+      // Implement code for acquiring the lock. This means requesting the token
+      // from all other clients, if we do not already have it.
+      if(state == NO_TOKEN) {
+         for(int i = 0; i < number_of_clients; i++) {
+            if(i != my_id) { // do not request from myself
+               objArray[i].request_token(my_id, token[my_id] + 1);
+            }
+         }
+         // wait until we receive token
+         while(true) {
+            if(state == TOKEN_HELD) {
+               break;
+            }
+            else {
+               try {
+                  Thread.sleep(500);
+               }
+               catch(InterruptedException e) {
+                  System.err.println("sleep error");
+               }
+            }
+         }
+      }
+      state = TOKEN_HELD;
+      // enter critical section
     }
     
 /*
-	This method is called from the local client whenever it wants to release
-	an acquired lock.
+   This method is called from the local client whenever it wants to release
+   an acquired lock.
 */
     public void release() {
 
-		//
-		// TODO:
-		// Implement code for releasing the lock.
-		// If a remote object wants the token, send it!
-		state = TOKEN_PRESENT;
-		release_token();
-	
-		System.out.println("Lock released!");
+      //
+      // TODO:
+      // Implement code for releasing the lock.
+      // If a remote object wants the token, send it!
+      state = TOKEN_PRESENT;
+      release_token();
+   
+      System.out.println("Lock released!");
     }
 
 
-	//
-	// TODO:
-	// Declare necessary (if any) local methods
-	public void release_token() {
-	   if(DEBUG) {
+   //
+   // TODO:
+   // Declare necessary (if any) local methods
+   public void release_token() {
+      if(DEBUG) {
          System.out.println("release_token called from my_id: " + my_id);
          System.out.println("state: " + state);
          for(int i = 0; i < number_of_clients; i++) {
@@ -227,15 +227,15 @@ public class Lock_impl extends LockPOA
          }
       }
 
-	   for(int i = 1; i <= number_of_clients; i++) {
-	      int k = (my_id + i) % number_of_clients;
+      for(int i = 1; i <= number_of_clients; i++) {
+         int k = (my_id + i) % number_of_clients;
          if(request[k] > token[k]) {
             state = NO_TOKEN;
             token[my_id] = token[my_id] + 1;
             objArray[k].acquire_token(token);
             break;
          }
-	   }
-	}
+      }
+   }
 
 }
